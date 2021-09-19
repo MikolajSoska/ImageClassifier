194ee7f18b7b982fdb9b9b4980c066256526b49d
Mikołaj Soska <miki_2007@gazeta.pl>
2021-09-19 10:35:17 +0200

    Added code for running training script.

---
+++ classifier/run_train.py
@@ -0,0 +1,46 @@
+import argparse
+
+import torch
+import torch.nn as nn
+from torchvision.models import resnet50
+
+from classifier.data import load_data
+from classifier.trainer import Trainer
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Parameters for training image classifier.')
+    parser.add_argument('--images-path', type=str, required=True, help='Path to training images directory')
+    parser.add_argument('--epochs', type=int, default=5, help='Training epochs')
+    parser.add_argument('--batch', type=int, default=16, help='Batch size')
+    parser.add_argument('--val-ratio', type=float, default=0.15, help='How much training data use for validation')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--adam-betas', type=float, default=(0.9, 0.999), nargs=2, help='Betas for Adam optimizer')
+    parser.add_argument('--adam-eps', type=float, default=1e-8, help='Epsilon value for Adam optimizer')
+    parser.add_argument('--use-gpu', action='store_true', help='Train with CUDA')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    train_loader, validation_loader, classes_number = load_data(args.images_path, args.batch, args.val_ratio)
+    model = resnet50(pretrained=False, num_classes=classes_number)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=args.adam_betas, eps=args.adam_eps)
+
+    trainer = Trainer(
+        model=model,
+        criterion=criterion,
+        optimizer=optimizer,
+        train_data=train_loader,
+        val_data=validation_loader,
+        epochs=args.epochs,
+        use_cuda=args.use_gpu
+    )
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main()
--- classifier/trainer.py
+++ classifier/trainer.py
@@ -1,42 +1,41 @@
 import torch
 import torch.nn as nn
 from sklearn.metrics import accuracy_score
-from torchvision.models import resnet50
+from torch.utils.data.dataloader import DataLoader
 
 import classifier.utils as utils
-from classifier.data import load_data
 
 
 class Trainer:
-    def __init__(self, path_to_data: str, batch_size: int, validation_ratio: float, learning_rate: float,
-                 use_cuda: bool):
-        self.path_to_data = path_to_data
-        self.batch_size = batch_size
-        self.validation_ratio = validation_ratio
-        self.learning_rate = learning_rate
+    def __init__(self, model: nn.Module, criterion: nn.Module, optimizer: torch.optim.Optimizer, train_data: DataLoader,
+                 val_data: DataLoader, epochs: int, use_cuda: bool):
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.train_data = train_data
+        self.val_data = val_data
+        self.epochs = epochs
         self.device = utils.get_device(use_cuda)
 
     def train(self):
-        train_loader, validation_loader, classes_number = load_data(self.path_to_data, self.batch_size,
-                                                                    self.validation_ratio)
+        self.model = self.model.to(self.device)
+        self.model.train()
+        for epoch in range(self.epochs):
+            self.__train_epoch(epoch)
 
-        model = resnet50(pretrained=False, num_classes=classes_number)
-        model = model.to(self.device)
-        criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=self.learning_rate)
-
+    def __train_epoch(self, epoch: int):
         running_loss = []
         running_score = []
-        train_size = len(train_loader)
-        for i, (data, labels) in enumerate(train_loader):
-            optimizer.zero_grad()
+        train_size = len(self.train_data)
+        for i, (data, labels) in enumerate(self.train_data):
+            self.optimizer.zero_grad()
             data = data.to(self.device)
             labels = labels.to(self.device)
 
-            prediction = model(data)
-            loss = criterion(prediction, labels)
+            prediction = self.model(data)
+            loss = self.criterion(prediction, labels)
             loss.backward()
-            optimizer.step()
+            self.optimizer.step()
 
             running_loss.append(loss.item())
             score = accuracy_score(torch.argmax(prediction, dim=-1).cpu(), labels.cpu())
@@ -45,5 +44,5 @@ class Trainer:
             if i % 50 == 0:
                 mean_loss = sum(running_loss) / len(running_loss)
                 mean_score = sum(running_score) / len(running_score)
-                print(f'Iteration: {i}/{train_size}, Loss: {mean_loss}, Accuracy: {mean_score}')
+                print(f'Epoch: {epoch}, Iteration: {i}/{train_size}, Loss: {mean_loss}, Accuracy: {mean_score}')
                 running_loss.clear()
